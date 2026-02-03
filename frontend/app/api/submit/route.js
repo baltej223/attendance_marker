61fcdcdc55f7edea41d3df556c41a42bca2cd8cc
baltej223 <baltej963@gmail.com>
2026-02-03 16:38:18 +0530

    Little fix

--- frontend/app/api/submit/route.js
+++ frontend/app/api/submit/route.js
@@ -2,30 +2,29 @@ import { NextResponse } from 'next/server';
 import { connectDB, User, UserData, attendance } from '@/database.js';
 
 await connectDB();
-
-let _1 = await attendance.findOne({time:"8:00"}).exec();
-let _2 = await attendance.findOne({time:"8:50"}).exec();
-let _3 = await attendance.findOne({time:"9:40"}).exec();
-let _4 = await attendance.findOne({time:"10:30"}).exec();
-let _5 = await attendance.findOne({time:"11:20"}).exec();
-let _6 = await attendance.findOne({time:"12:10"}).exec();
-let _7 = await attendance.findOne({time:"1:50"}).exec();
-let _8 = await attendance.findOne({time:"2:40"}).exec();
-let _9 = await attendance.findOne({time:"3:30"}).exec();
-let _10 = await attendance.findOne({time:"4:20"}).exec();
-let _11 = await attendance.findOne({time:"5:10"}).exec();
-
-const times = ["8:00", "8:50", "9:40", "10:30", "11:20", "12:10", "1:50", "2:40", "3:30", "4:20", "5:10"];
-const attendanceRecords = [_1, _2, _3, _4, _5, _6, _7, _8, _9, _10, _11];
-
-for (let i = 0; i < attendanceRecords.length; i++) {
-  if (!attendanceRecords[i]) {
-  await new attendance({
-    time: times[i],
-    links: []
-  }).save();
-  }
-}
+// let _1 = await attendance.findOne({time:"8:00"}).exec();
+// let _2 = await attendance.findOne({time:"8:50"}).exec();
+// let _3 = await attendance.findOne({time:"9:40"}).exec();
+// let _4 = await attendance.findOne({time:"10:30"}).exec();
+// let _5 = await attendance.findOne({time:"11:20"}).exec();
+// let _6 = await attendance.findOne({time:"12:10"}).exec();
+// let _7 = await attendance.findOne({time:"1:50"}).exec();
+// let _8 = await attendance.findOne({time:"2:40"}).exec();
+// let _9 = await attendance.findOne({time:"3:30"}).exec();
+// let _10 = await attendance.findOne({time:"4:20"}).exec();
+// let _11 = await attendance.findOne({time:"5:10"}).exec();
+//
+// const times = ["8:00", "8:50", "9:40", "10:30", "11:20", "12:10", "1:50", "2:40", "3:30", "4:20", "5:10"];
+// const attendanceRecords = [_1, _2, _3, _4, _5, _6, _7, _8, _9, _10, _11];
+//
+// for (let i = 0; i < attendanceRecords.length; i++) {
+//   if (!attendanceRecords[i]) {
+//   await new attendance({
+//     time: times[i],
+//     links: []
+//   }).save();
+//   }
+// }
 
 
 export async function POST(req) {
